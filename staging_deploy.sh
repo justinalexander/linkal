8b8601a1c29e313f57b096fd67f7c7453d9d27d9
Flaviu Simihaian <flsimihaian@gmail.com>
2012-06-08 08:30:25 -0400

    tweaks to fix heroku deployment

--- staging_deploy.sh
+++ staging_deploy.sh
@@ -1,12 +1,11 @@
 #/bin/bash
-git push origin staging
+git push origin master
 git push staging staging:master
-heroku pgbackups:capture --expire --app socialatitude
-heroku rake db:migrate --app socialatitude
-heroku restart --app socialatitude
+heroku pgbackups:capture --expire --app linkal
+heroku run rake db:migrate --app linkal
+heroku restart --app linkal
 
 #after restart, heroku always throws an error, so curl to bypass that
-curl socialatitude.heroku.com > /dev/null 2> /dev/null
-curl socialatitude.heroku.com > /dev/null 2> /dev/null
+curl linkal.heroku.com > /dev/null 2> /dev/null
+curl linkal.heroku.com > /dev/null 2> /dev/null
 exit 0
-
