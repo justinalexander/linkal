192de8abfb8260ab05471b3e61b2cd9b6cbd179b
Valentin Zberea <valentin.zberea@gmail.com>
2012-06-14 10:34:33 +0300

    modifications for staging deploy

--- sync_staging_database.sh
+++ sync_staging_database.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 #Restores productions data into the staging environment
 
-heroku pgbackups:restore SHARED_DATABASE `heroku pgbackups:url --app socialatitude-prod` --app socialatitude
+heroku pgbackups:restore SHARED_DATABASE `heroku pgbackups:url --app linkal` --app linkal
 exit 0
