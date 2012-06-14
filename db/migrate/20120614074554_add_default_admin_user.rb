4bc4c48b030928b657905da27b9ebc5bcc13079c
Valentin Zberea <valentin.zberea@gmail.com>
2012-06-14 11:09:41 +0300

    removed add_default_admin_user migration

--- db/migrate/20120614074554_add_default_admin_user.rb
+++
@@ -1,8 +0,0 @@
-class AddDefaultAdminUser < ActiveRecord::Migration
-  def self.up
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
-  end
-
-  def self.down
-  end
-end
