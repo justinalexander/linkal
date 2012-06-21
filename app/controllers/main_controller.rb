bef9bc31be5c84a7b57e2bd0ed935a3cffb5642a
Valentin Zberea <valentin.zberea@gmail.com>
2012-06-21 12:23:38 +0300

    all events views finished

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,25 +9,25 @@ class MainController < ApplicationController
   end
   def my_events_calendar
     set_events_url
-    if params[:year].nil? or params[:month].nil? or params[:day].nil?
-      now = Time.now
-      @current_date = Time.new(now.year, now.month, now.day)
-    else
-      @current_date = Time.new(params[:year], params[:month], params[:day])
-    end
-    @date_events = Event.from_organizations_followed_by(current_user)
-      .between(@current_date.beginning_of_day, @current_date.end_of_day)
-    @month_events = Event.from_organizations_followed_by(current_user)
-      .between(@current_date.beginning_of_month, @current_date.end_of_month)
+    set_current_date
+
+    set_calendar_events_for Event.from_organizations_followed_by(current_user)
+
     ensure_followed_organizations!
   end
 
   def all_events
     set_events_url
+    @events = Event.upcoming
     ensure_followed_organizations!
   end
+
   def all_events_calendar
     set_events_url
+    set_current_date
+
+    set_calendar_events_for Event
+
     ensure_followed_organizations!
   end
 
@@ -38,5 +38,17 @@ class MainController < ApplicationController
   private
   def set_events_url
     session["events_url"] = url_for()
+  end
+  def set_current_date
+    if params[:year].nil? or params[:month].nil? or params[:day].nil?
+      now = Time.now
+      @current_date = Time.new(now.year, now.month, now.day)
+    else
+      @current_date = Time.new(params[:year], params[:month], params[:day])
+    end
+  end
+  def set_calendar_events_for events
+    @date_events = events.between(@current_date.beginning_of_day, @current_date.end_of_day)
+    @month_events = events.between(@current_date.beginning_of_month, @current_date.end_of_month)
   end
 end
