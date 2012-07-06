ca2fd57ad6f860360c6fb76a2ca7270c12666e8a
Valentin Zberea <valentin.zberea@gmail.com>
2012-07-07 00:01:22 +0300

    fixed root url on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -36,7 +36,7 @@ Socialatitude::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'socialatitude.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'linkal.heroku.com' }
   config.action_mailer.delivery_method = :ses
 
   # Enable threaded mode
