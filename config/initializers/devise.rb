3730c2cbfbd862d7d1f31e705ff36163206c1f56
Flaviu Simihaian <flsimihaian@gmail.com>
2012-07-12 15:00:21 -0400

    added amazon from emails for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "no-reply@socialatitude.com"
+  config.mailer_sender = "events@senkd.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
