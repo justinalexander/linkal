7d224d03d16a0596dff1804b70f02d87fdd67475
Valentin Zberea <valentin.zberea@gmail.com>
2012-07-22 22:23:08 +0300

    no messages when redirected to login page

--- lib/secure_failure_app.rb
+++ lib/secure_failure_app.rb
@@ -3,9 +3,19 @@ class SecureFailureApp < Devise::FailureApp
   # Use full URL instead of path for authentication failure redirect
   def redirect_url
     if skip_format?
-      send(:"new_#{scope}_session_url")
+      if scope == :user
+        flash.clear()
+        send(:"login_url")
+      else
+        send(:"new_#{scope}_session_url")
+      end
     else
-      send(:"new_#{scope}_session_url", :format => request_format)
+      if scope == :user
+        flash.clear()
+        send(:"login_url", :format => request_format)
+      else
+        send(:"new_#{scope}_session_url", :format => request_format)
+      end
     end
   end
 
