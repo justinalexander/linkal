466fb0d533db152cf5c8ac4b04f5304a4de9b3e4
justinalexander <justin@socialatitude.com>
2012-08-07 13:03:44 -0400

    change email events from 2 weeks out to 1 week out

--- app/mailers/event_updates.rb
+++ app/mailers/event_updates.rb
@@ -2,7 +2,7 @@ class EventUpdates < ActionMailer::Base
   default :from => "events@senkd.com"
   def weekly_email(user)
     if user.weekly_email == true
-      two_weeks_from_now = Time.now + (2*7*24*60*60)
+      two_weeks_from_now = Time.now + (1*7*24*60*60)
       @events = Event.upcoming.from_organizations_followed_by(user).where('start_at < ?', two_weeks_from_now)
       mail(:to => user.email, :subject => 'Upcoming events')
     end
